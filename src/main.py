18e38533e7e52f27d8d9554fcee791c99f8f6fda
jgatetekez-code <j.gatetekez@alustudent.com>
2026-09-06 18:16:20 +0200

    Add ALU email extraction and validation

--- src/main.py
+++ src/main.py
@@ -6,5 +6,21 @@ with open("input/raw-text.txt", "r") as file:
 print(text)
 
 
-emails = re.findall("[a-z0-9.]+@[a-z0-9.]+", text)
+emails = re.findall(r"[a-z0-9._+-]+@[a-z0-9.-]+\.[a-z]{2,}", text)
+
 print(emails)
+
+valid_emails = []
+
+for email in emails:
+    if (
+        email.endswith("@alueducation.com")
+        or email.endswith("@alumni.alueducation.com")
+        or email.endswith("@si.alueducation.com")
+    ):
+        valid_emails.append(email)
+
+print(valid_emails)
+
+
+            
